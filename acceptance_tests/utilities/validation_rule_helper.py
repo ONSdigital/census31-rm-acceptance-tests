1a4e55325a8b27a34683242aa3933cf13ed3ed44
Ryan Grundy <ryan.grundy@ons.gov.uk>
2026-07-30 16:15:30 +0100

    CN-136 Update tests to use new sample structure

--- acceptance_tests/utilities/validation_rule_helper.py
+++ acceptance_tests/utilities/validation_rule_helper.py
@@ -2,15 +2,15 @@ import csv
 from pathlib import Path
 
 
-def get_sample_rows_and_generate_open_validation_rules(sample_file_path: Path):
-    sample_header, sample_rows = get_sample_header_and_rows(sample_file_path)
+def get_sample_rows_and_generate_open_validation_rules(sample_file_path: Path, delimiter: str = '|'):
+    sample_header, sample_rows = get_sample_header_and_rows(sample_file_path, delimiter=delimiter)
 
     return sample_rows
 
 
-def get_sample_header_and_rows(sample_file_path: Path):
+def get_sample_header_and_rows(sample_file_path: Path, delimiter: str):
     with open(sample_file_path) as sample_file:
-        reader = csv.DictReader(sample_file)
+        reader = csv.DictReader(sample_file, delimiter=delimiter)
         sample_header = reader.fieldnames
         sample_rows = [row for row in reader]
     return sample_header, sample_rows
